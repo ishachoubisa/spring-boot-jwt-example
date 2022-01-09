423d5521dbd57be7e86d73b26cbff04c81e3224f
ishachoubisa <isha91choubisa1234@gmail.com>
2022-01-10 00:58:07 +0100

    added missed gits

--- src/main/java/com/learn/isha/java/jwt/config/SecurityConfigurationClass.java
+++ src/main/java/com/learn/isha/java/jwt/config/SecurityConfigurationClass.java
@@ -1,4 +1,22 @@
 package com.learn.isha.java.jwt.config;
 
-public class SecurityConfigurationClass {
+import com.learn.isha.java.jwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurationClass extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
 }
--- src/main/java/com/learn/isha/java/jwt/service/UserService.java
+++ src/main/java/com/learn/isha/java/jwt/service/UserService.java
@@ -1,4 +1,20 @@
 package com.learn.isha.java.jwt.service;
 
-public class UserService {
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //Logic to load user from database but for demo we are returning mock user
+        return new User("admin","password",new ArrayList<>());
+    }
 }
